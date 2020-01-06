749690878d20b4f62fc4f6245d46d64131fc5248
artur rachwal <artur.rachwal@contractors.roche.com>
2020-01-06 23:28:06 +0100

    more testing and working flow

--- src/Flow/FlowItem.cs
+++ src/Flow/FlowItem.cs
@@ -1,28 +1,20 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace Flow
 {
     public class FlowItem<T> : IBeginFlow<T>, IValidatedVerified<T>
     {
-        private readonly List<Action<Stack>> _actions;
-        private readonly Stack _resultStack;
+        private readonly Action<IFlowItemToken> _action;
 
         public FlowItem(T input)
-            : this(input, new List<Action<Stack>>(), new Stack())
+            : this(x => x.PushResult(input))
         {
         }
 
-        private FlowItem(List<Action<Stack>> actions, Stack resultStack)
+        private FlowItem(Action<IFlowItemToken> action)
         {
-            _actions = actions;
-            _resultStack = resultStack;
-        }
-        private FlowItem(T input, List<Action<Stack>> actions, Stack resultStack)
-            : this(actions, resultStack)
-        {
-            _resultStack.Push(input);
+            _action = action;
         }
 
         public IBeginFlow<T> Validate<TR>(Func<T, TR> validationTarget, Func<TR, bool> validator, Func<IError> error)
@@ -32,7 +24,17 @@ namespace Flow
 
         public IBeginFlow<T> Validate(Func<T, bool> validator, Func<IError> error)
         {
-            throw new NotImplementedException();
+            void NextAction(IFlowItemToken x)
+            {
+                _action(x);
+                T target = (T)x.CurrentResult;
+                if (!validator(target))
+                {
+                    x.PushError(error());
+                }
+            }
+
+            return new FlowItem<T>(NextAction);
         }
 
         public IBeginFlow<T> Validate<TR>(Func<T, TR> validationTarget, IFilter<TR> filter)
@@ -47,8 +49,14 @@ namespace Flow
 
         public IValidatedVerified<TR> Apply<TR>(Func<T, TR> apply)
         {
-            _actions.Add(stack => stack.Push(apply((T)stack.Peek())));
-            return new FlowItem<TR>(_actions, _resultStack);
+            void NextAction(IFlowItemToken x)
+            {
+                _action(x);
+                T target = (T)x.CurrentResult;
+                x.PushResult(apply(target));
+            }
+
+            return new FlowItem<TR>(NextAction);
         }
 
         public IValidated<T> Verify<TR>(Func<T, TR> verificationTarget, Func<TR, bool> check, Func<IError> error)
@@ -58,7 +66,17 @@ namespace Flow
 
         public IValidated<T> Verify(Func<T, bool> check, Func<IError> error)
         {
-            throw new NotImplementedException();
+            void NextAction(IFlowItemToken x)
+            {
+                _action(x);
+                T target = (T)x.CurrentResult;
+                if (!check(target))
+                {
+                    x.PushError(error());
+                }
+            }
+
+            return new FlowItem<T>(NextAction);
         }
 
         public IValidated<T> Verify(IFilter<T> filter)
@@ -73,14 +91,32 @@ namespace Flow
 
         public IPipeline Finalize(Action<T> execution)
         {
-            _actions.Add(stack => execution((T)stack.Peek()));
-            return new Pipeline(_actions, _resultStack);
+            void NextAction(IFlowItemToken x)
+            {
+                _action(x);
+                if (!x.Errors.Any())
+                {
+                    T target = (T)x.CurrentResult;
+                    execution(target);
+                }
+            }
+
+            return new Pipeline(NextAction);
         }
 
         public IPipeline<TR> Finalize<TR>(Func<T, TR> execution)
         {
-            _actions.Add(stack => _resultStack.Push(execution((T)stack.Peek())));
-            return new Pipeline<TR>(_actions, _resultStack);
+            void NextAction(IFlowItemToken x)
+            {
+                _action(x);
+                if (!x.Errors.Any())
+                {
+                    T target = (T)x.CurrentResult;
+                    x.PushResult(execution(target));
+                }
+            }
+
+            return new Pipeline<TR>(NextAction);
         }
     }
 }
---
+++ src/Flow/IFlowItemToken.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Flow
+{
+    public interface IFlowItemToken
+    {
+        object CurrentResult { get; }
+        void PushResult(object o);
+
+        IError CurrentError { get; }
+
+        void PushError(IError error);
+
+        IEnumerable<IError> Errors { get; }
+    }
+}
