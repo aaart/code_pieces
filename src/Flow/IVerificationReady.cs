60016199a35d9a45f6ef365066bde91eeca547aa
artur rachwal <artur.rachwal@gmail.com>
2020-01-16 22:30:30 +0100

    multiple verifications can be done

--- src/Flow/IVerificationReady.cs
+++ src/Flow/IVerificationReady.cs
@@ -4,9 +4,9 @@ namespace Flow
 {
     public interface IVerificationReady<out T>
     {
-        IValidated<T> Verify<TR>(Func<T, TR> transform, Func<TR, bool> check, Func<IError> error);
-        IValidated<T> Verify(Func<T, bool> check, Func<IError> error);
-        IValidated<T> Verify(IFilter<T> filter);
-        IValidated<T> Verify<TR>(Func<T, TR> transform, IFilter<TR> filter);
+        IValidatedVerified<T> Verify<TR>(Func<T, TR> transform, Func<TR, bool> check, Func<IError> error);
+        IValidatedVerified<T> Verify(Func<T, bool> check, Func<IError> error);
+        IValidatedVerified<T> Verify(IFilter<T> filter);
+        IValidatedVerified<T> Verify<TR>(Func<T, TR> transform, IFilter<TR> filter);
     }
 }
