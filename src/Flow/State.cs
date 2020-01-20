326e90cd95acf1130f464b92e2360a7b756e6f20
artur rachwal <artur.rachwal@gmail.com>
2020-01-20 22:18:44 +0100

    ContructorProxy method aadded to hide constructors

--- src/Flow/State.cs
+++ src/Flow/State.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 
 namespace Flow
 {
-    public class State : IState
+    public class State<TEventReceiver> : IState
+        where TEventReceiver : IEventReceiver
     {
-        protected static State ConstructorProxy(IEnumerable<IError> errors, IEventReceiver eventReceiver, Action<IEnumerable<IError>, IEventReceiver> onDisposing) => 
-            new State(errors, eventReceiver, onDisposing);
+        protected static State<TEventReceiver> ConstructorProxy(IEnumerable<IError> errors, TEventReceiver eventReceiver, Action<IEnumerable<IError>, TEventReceiver> onDisposing) =>
+            new State<TEventReceiver>(errors, eventReceiver, onDisposing);
 
-        protected IEventReceiver EventReceiver { get; }
+        protected TEventReceiver EventReceiver { get; }
         private readonly List<IError> _errors = new List<IError>();
-        protected Action<IEnumerable<IError>, IEventReceiver> OnDisposing { get; }
-
-        public State()
-            : this(new List<IError>(), new StandardEventReceiver(), (e, er) => { })
-        {
-        }
-
-        protected State(IEnumerable<IError> errors, IEventReceiver eventReceiver, Action<IEnumerable<IError>, IEventReceiver> onDisposing)
+        protected Action<IEnumerable<IError>, TEventReceiver> OnDisposing { get; }
+        
+        protected State(IEnumerable<IError> errors, TEventReceiver eventReceiver, Action<IEnumerable<IError>, TEventReceiver> onDisposing)
         {
             EventReceiver = eventReceiver;
             _errors.AddRange(errors);
@@ -30,25 +26,23 @@ namespace Flow
 
         public void PublishError(IError error) => _errors.Add(error);
 
-        public void Receive(IEvent @event)
-        {
-            throw new System.NotImplementedException();
-        }
+        public void Receive(IEvent @event) => throw new System.NotImplementedException();
 
-        public void Dispose()
-        {
-            OnDisposing(Errors, EventReceiver);
-        }
+        public void Dispose() => OnDisposing(Errors, EventReceiver);
     }
 
-    public class State<T> : State, IState<T>
+    public class State<T, TEventReceiver> : State<TEventReceiver>, IState<T>
+        where TEventReceiver : IEventReceiver
     {
-        public State(T result)
-            : this(result, new List<IError>(), new StandardEventReceiver(), (e, er) => { })
+        protected static State<TR, TEventReceiver> ConstructorProxy<TR>(TR result, IEnumerable<IError> errors, TEventReceiver eventReceiver, Action<IEnumerable<IError>, TEventReceiver> onDisposing) =>
+            new State<TR, TEventReceiver>(result, errors, eventReceiver, onDisposing);
+
+        public State(T result, TEventReceiver eventReceiver)
+            : this(result, new List<IError>(), eventReceiver, (e, er) => { })
         {
         }
 
-        protected State(T result, IEnumerable<IError> errors, IEventReceiver eventReceiver, Action<IEnumerable<IError>, IEventReceiver> onDisposing)
+        protected State(T result, IEnumerable<IError> errors, TEventReceiver eventReceiver, Action<IEnumerable<IError>, TEventReceiver> onDisposing)
             : base(errors, eventReceiver, onDisposing)
         {
             Result = result;
@@ -56,19 +50,10 @@ namespace Flow
 
         public T Result { get; }
 
-        public IState<TR> Next<TR>(TR result)
-        {
-            return new State<TR>(result, Errors, EventReceiver, OnDisposing);
-        }
+        public IState<TR> Next<TR>(TR result) => ConstructorProxy(result, Errors, EventReceiver, OnDisposing);
 
-        public IState<TR> Skip<TR>()
-        {
-            return new State<TR>(default, Errors, EventReceiver, OnDisposing);
-        }
+        public IState<TR> Skip<TR>() => ConstructorProxy<TR>(default, Errors, EventReceiver, OnDisposing);
 
-        public IState Next()
-        {
-            return ConstructorProxy(Errors, EventReceiver, OnDisposing);
-        }
+        public IState Next() => ConstructorProxy(Errors, EventReceiver, OnDisposing);
     }
 }
