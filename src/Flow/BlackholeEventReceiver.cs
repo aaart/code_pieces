21168b72d32328aa0840a0b94e242c33d6bd68c9
artur rachwal <artur.rachwal@contractors.roche.com>
2020-01-26 20:15:08 +0100

    blackhole event receiver provided

---
+++ src/Flow/BlackholeEventReceiver.cs
@@ -0,0 +1,10 @@
+﻿namespace Flow
+{
+    public class BlackholeEventReceiver : IEventReceiver
+    {
+        public void Receive<TE>(TE e) 
+            where TE : IEvent
+        {
+        }
+    }
+}
--- src/Flow/StandardEventReceiver.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Flow
-{
-    public class StandardEventReceiver : IEventReceiver
-    {
-        public void Receive<TE>(TE e) where TE : IEvent
-        {
-        }
-    }
-}
