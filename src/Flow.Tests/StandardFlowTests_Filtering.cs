7b16cd6746406e37d469d62fe544ddd8ca7d6126
artur rachwal <artur.rachwal@gmail.com>
2020-01-26 16:03:17 +0100

    pipeline validation moveed to dedicated file

---
+++ src/Flow.Tests/StandardFlowTests_Filtering.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Flow.Tests.TestUtilities;
+using Xunit;
+
+namespace Flow.Tests
+{
+    public partial class StandardFlowTests
+    {
+        [Fact]
+        public void Flow_WhenValidationFails_ExpectSingleErrorInResult()
+        {
+            var (_, _, filteringErrors) = _builder
+                .For(new { Msg = "Mockery" })
+                .Validate(x => false, () => new TestingFilteringError())
+                .Finalize(Predefined.EmptyMethod)
+                .Sink();
+            Assert.Equal(1, filteringErrors.Count);
+        }
+
+        [Fact]
+        public void Flow_WhenValidationFails_ExpectSingleErrorInResult2()
+        {
+            var (_, _, filteringErrors) = _builder
+                .For(new { Msg = "Mockery" })
+                .Validate(x => false, () => new TestingFilteringError())
+                .Finalize(x => false)
+                .Sink();
+            Assert.Equal(1, filteringErrors.Count);
+        }
+
+        [Fact]
+        public void Flow_WhenValidationFails_ExpectSingleErrorInResult3()
+        {
+            var (_, _, filteringErrors) = _builder
+                .For(new { Msg = "Mockery" })
+                .Validate(x => false, () => new TestingFilteringError())
+                .Finalize(x => false)
+                .Project(x => 1)
+                .Sink();
+            Assert.Equal(1, filteringErrors.Count);
+        }
+
+        [Fact]
+        public void Flow_WhenVerificationFails_ExpectSingleErrorInResult()
+        {
+            var (_, _, filteringErrors) = _builder
+                .For(new { Msg = "Mockery" })
+                .Apply(x => x)
+                .Verify(x => false, () => new TestingFilteringError())
+                .Finalize(Predefined.EmptyMethod)
+                .Sink();
+            Assert.Equal(1, filteringErrors.Count);
+        }
+
+
+        [Fact]
+        public void Flow_WhenValidationFails_PipelineStopped()
+        {
+            bool executed = false;
+            _builder
+                .For(new { Msg = "Mockery" })
+                .Validate(x => false, () => new TestingFilteringError())
+                .Finalize(x =>
+                {
+                    executed = true;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenValidationFails_PipelineStopped2()
+        {
+            bool executed = false;
+            _builder
+                .For(new { Msg = "Mockery" })
+                .Validate(x => x, x => false, () => new TestingFilteringError())
+                .Finalize(x =>
+                {
+                    executed = true;
+                    return x;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenValidationFails_PipelineStopped3()
+        {
+            bool executed = false;
+            _builder
+                .For(new { Msg = "Mockery" })
+                .Validate(x => new TestingInput(), new TestingFilter())
+                .Finalize(x =>
+                {
+                    executed = true;
+                    return x;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenValidationFails_PipelineStopped4()
+        {
+            bool executed = false;
+            _builder
+                .For(new TestingInput())
+                .Validate(new TestingFilter())
+                .Finalize(x =>
+                {
+                    executed = true;
+                    return x;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenValidationChangesTarget_ExpectFinalizeExecuted()
+        {
+            bool executed = false;
+            _builder
+                .For(new { Prop = true })
+                .Validate(x => x.Prop, x => x, () => new TestingFilteringError())
+                .Finalize(x =>
+                {
+                    executed = true;
+                })
+                .Sink();
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenVerificationFails_PipelineStopped()
+        {
+            bool executed = false;
+            _builder
+                .For(new { Msg = "Mockery" })
+                .Apply(x => x)
+                .Verify(x => false, () => new TestingFilteringError())
+                .Finalize(x =>
+                {
+                    executed = true;
+                    return x;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenVerificationFails_PipelineStopped2()
+        {
+            bool executed = false;
+            _builder
+                .For(new { Msg = "Mockery" })
+                .Apply(x => x)
+                .Verify(x => x.Msg, x => false, () => new TestingFilteringError())
+                .Finalize(x =>
+                {
+                    executed = true;
+                    return x;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenVerificationFails_PipelineStopped3()
+        {
+            bool executed = false;
+            _builder
+                .For(new { Msg = "Mockery" })
+                .Apply(x => x)
+                .Verify(x => new TestingInput(), new TestingFilter())
+                .Finalize(x =>
+                {
+                    executed = true;
+                    return x;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void Flow_WhenVerificationFails_PipelineStopped4()
+        {
+            bool executed = false;
+            _builder
+                .For(new TestingInput())
+                .Apply(x => x)
+                .Verify(new TestingFilter())
+                .Finalize(x =>
+                {
+                    executed = true;
+                    return x;
+                })
+                .Sink();
+            Assert.False(executed);
+        }
+    }
+}
