cb0ce4a0852b5d91da836d3e3bc9c3f9155a7dd3
artur rachwal <artur.rachwal@contractors.roche.com>
2020-01-27 15:06:03 +0100

    IEventPublisher -> IEventSource

--- src/Flow/IEventPublisher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Flow
-{
-    public interface IEventPublisher<out T>
-    {
-        IValidatedVerified<T> Publish(Action<T, IEventReceiver> publishingMethod);
-    }
-}
---
+++ src/Flow/IEventSource.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Flow
+{
+    public interface IEventSource<out T>
+    {
+        IValidatedVerified<T> Raise(Action<T, IEventReceiver> publishingMethod);
+    }
+}
