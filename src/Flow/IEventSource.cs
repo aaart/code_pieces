8b655e0cd6702803bbc8d4c17a820eb7f57d03a2
artur rachwal <artur.rachwal@contractors.roche.com>
2020-01-27 22:57:20 +0100

    minor

--- src/Flow/IEventSource.cs
+++ src/Flow/IEventSource.cs
@@ -4,6 +4,6 @@ namespace Flow
 {
     public interface IEventSource<out T>
     {
-        IValidatedVerified<T> Raise(Action<T, IEventReceiver> publishingMethod);
+        IValidatedVerified<T> Raise(Action<T, IEventReceiver> raise);
     }
 }
