62ac342e7d58c4cd616caef342b90324d3be0fb4
artur rachwal <artur.rachwal@contractors.roche.com>
2020-02-01 13:08:06 +0100

    immediate publisher added

---
+++ src/Flow/ImmediatePublishEventReceiver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Flow
+{
+    public abstract class ImmediatePublishEventReceiver : IEventReceiver
+    {
+        public void Receive<TEvent>(TEvent e)
+        {
+            CreatePublisher(e).Invoke();
+        }
+
+        protected abstract Action CreatePublisher<TEvent>(TEvent e);
+
+        public void Dispose()
+        {
+        }
+    }
+}
