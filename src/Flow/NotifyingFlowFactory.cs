6797282a17e592c436bcb667082c61f19880c402
artur rachwal <artur.rachwal@gmail.com>
2020-02-02 15:13:35 +0100

    logging minor upgrade

--- src/Flow/NotifyingFlowFactory.cs
+++ src/Flow/NotifyingFlowFactory.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Flow
 {
     public class NotifyingFlowFactory<TFilteringError> : INotifyingFlowFactory<TFilteringError>
     {
         private readonly IEventReceiverFactory _eventReceiverFactory;
+        private readonly ILogger _logger;
         private readonly ILoggerFactory _loggerFactory;
 
+        public NotifyingFlowFactory(IEventReceiverFactory eventReceiverFactory)
+        {
+            _eventReceiverFactory = eventReceiverFactory;
+            _logger = NullLogger.Instance;
+        }
+
+        public NotifyingFlowFactory(IEventReceiverFactory eventReceiverFactory, ILogger logger)
+        {
+            _eventReceiverFactory = eventReceiverFactory;
+            _logger = logger;
+        }
+        
         public NotifyingFlowFactory(IEventReceiverFactory eventReceiverFactory, ILoggerFactory loggerFactory)
         {
             _eventReceiverFactory = eventReceiverFactory;
@@ -18,7 +32,7 @@ namespace Flow
                 () => new State<T, TFilteringError>(
                     target, 
                     new StateData<TFilteringError>(
-                            _loggerFactory.CreateLogger(typeof(Step<T, TFilteringError>)),
+                            _logger ?? _loggerFactory.CreateLogger<Step<T, TFilteringError>>(),
                             _eventReceiverFactory.Create())));
     }
 }
