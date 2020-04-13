09150ab0a639a82542ad8c44a3e06afffe7f00c7
artur rachwal <artur.rachwal@gmail.com>
2020-04-13 19:21:12 +0200

    tests done

--- src/PipeSharp.Tests/StandardFlowTests_OnDone.cs
+++ src/PipeSharp.Tests/StandardFlowTests_OnDone.cs
@@ -6,46 +6,46 @@ namespace PipeSharp.Tests
     public partial class StandardFlowTests
     {
         [Fact]
-        public void GivenFlow_WhenOnlyFinalizeDefined_ExpectSingleOnDoing()
+        public void GivenFlow_WhenOnlyFinalizeDefined_ExpectSingleOnDone()
         {
-            int onDoingCount = 0;
+            int onDoneCount = 0;
             _factory
-                .For(default(int), () => { onDoingCount++; }, () => { })
-                .Finalize(x => {})
+                .For(default(int), () => { }, () => { onDoneCount++; })
+                .Finalize(x => { })
                 .Sink();
-            Assert.Equal(1, onDoingCount);
-        }
-        
-        [Fact]
-        public void GivenFlow_WhenApplyAndFinalizeDefined_ExpectSingleOnDoing()
-        {
-            int onDoingCount = 0;
-            _factory
-                .For(default(int), () => { onDoingCount++; }, () => { })
-                .Apply(x => x)
-                .Apply(x => x)
-                .Apply(x => x)
-                .Apply(x => x)
-                .Apply(x => x)
-                .Apply(x => x)
-                .Apply(x => x)
-                .Apply(x => x)
-                .Apply(x => x)
-                .Finalize(x => {})
-                .Sink();
-            Assert.Equal(10, onDoingCount);
+            Assert.Equal(1, onDoneCount);
         }
 
         [Fact]
-        public void GivenFlow_WhenCheckingDefined_ExpectSingleOnDoing()
+        public void GivenFlow_WhenApplyAndFinalizeDefined_ExpectSingleOnDone()
         {
-            int onDoingCount = 0;
+            int onDoneCount = 0;
             _factory
-                .For(default(int), () => { onDoingCount++; }, () => { })
+                .For(default(int), () => { }, () => { onDoneCount++; })
+                .Apply(x => x)
+                .Apply(x => x)
+                .Apply(x => x)
+                .Apply(x => x)
+                .Apply(x => x)
+                .Apply(x => x)
+                .Apply(x => x)
+                .Apply(x => x)
+                .Apply(x => x)
+                .Finalize(x => { })
+                .Sink();
+            Assert.Equal(10, onDoneCount);
+        }
+
+        [Fact]
+        public void GivenFlow_WhenCheckingDefined_ExpectSingleOnDone()
+        {
+            int onDoneCount = 0;
+            _factory
+                .For(default(int), () => { }, () => { onDoneCount++; })
                 .Check(x => true, () => new TestingFilteringError())
                 .Finalize(x => { })
                 .Sink();
-            Assert.Equal(1, onDoingCount);
+            Assert.Equal(1, onDoneCount);
         }
     }
 }
