f9ae722b6ddc7f456cbf7cceb0a4a8fbd41e3a50
artur rachwal <artur.rachwal@gmail.com>
2020-04-14 09:47:50 +0200

    generic event receiver factory added

---
+++ src/PipeSharp/GenericEventReceiverFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace PipeSharp
+{
+    public class GenericEventReceiverFactory<T> : IEventReceiverFactory
+        where T : IEventReceiver, new()
+    {
+        public IEventReceiver Create() => new T();
+    }
+}
