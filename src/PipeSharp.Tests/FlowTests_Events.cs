bb0f1bf09895d2d9bf58bbc14d84db445cf3b8cd
artur rachwal <artur.rachwal@gmail.com>
2020-04-14 09:40:54 +0200

    tests refactored

---
+++ src/PipeSharp.Tests/FlowTests_Events.cs
@@ -0,0 +1,106 @@
+﻿using PipeSharp.Tests.TestUtilities;
+using Xunit;
+
+namespace PipeSharp.Tests
+{
+    public class FlowTests_Events
+    {
+        [Fact]
+        public void Flow_WhenTestingEventPublished_ExpectEventReceived1()
+        {
+            bool received = false;
+
+            new StandardBuilder()
+                .WithFilteringError<TestingFilteringError>()
+                .WithEvents(new TestingEventReceiverFactory(() => received = true, () => { }))
+                .For(default(int))
+                .Raise(x => new TestingEvent())
+                .Finalize(x => { })
+                .Sink();
+
+            Assert.True(received);
+        }
+
+        [Fact]
+        public void Flow_WhenTestingEventPublished_ExpectEventReceived2()
+        {
+            bool received = false;
+
+            new StandardBuilder()
+                .WithFilteringError<TestingFilteringError>()
+                .WithEvents(new TestingEventReceiverFactory(() => received = true, () => { }))
+                .For(default(int))
+                .Raise(x => new TestingEvent())
+                .Finalize(x => x)
+                .Sink();
+            
+            Assert.True(received);
+        }
+
+        [Fact]
+        public void Flow_WhenTestingEventPublished_ExpectEventReceived3()
+        {
+            bool received = false;
+
+            new StandardBuilder()
+                .WithFilteringError<TestingFilteringError>()
+                .WithEvents(new TestingEventReceiverFactory(() => received = true, () => { }))
+                .For(default(int))
+                .Raise(x => new TestingEvent())
+                .Finalize(x => x)
+                .Project(x => x)
+                .Sink();
+            
+            Assert.True(received);
+        }
+        
+        [Fact]
+        public void Flow_WhenTestingEventPublished_ExpectReceiverDisposed1()
+        {
+            bool disposed = false;
+
+            new StandardBuilder()
+                .WithFilteringError<TestingFilteringError>()
+                .WithEvents(new TestingEventReceiverFactory(() => { }, () => disposed = true))
+                .For(default(int))
+                .Raise(x => new TestingEvent())
+                .Finalize(x => { })
+                .Sink();
+
+            Assert.True(disposed);
+        }
+
+        [Fact]
+        public void Flow_WhenTestingEventPublished_ExpectReceiverDisposed2()
+        {
+            bool disposed = false;
+
+            new StandardBuilder()
+                .WithFilteringError<TestingFilteringError>()
+                .WithEvents(new TestingEventReceiverFactory(() => { }, () => disposed = true))
+                .For(default(int))
+                .Raise(x => new TestingEvent())
+                .Finalize(x => x)
+                .Sink();
+            
+            Assert.True(disposed);
+        }
+
+        [Fact]
+        public void Flow_WhenTestingEventPublished_ExpectReceiverDisposed3()
+        {
+            bool disposed = false;
+
+            new StandardBuilder()
+                .WithFilteringError<TestingFilteringError>()
+                .WithEvents(new TestingEventReceiverFactory(() => { }, () => disposed = true))
+                .For(default(int))
+                .Raise(x => new TestingEvent())
+                .Finalize(x => x)
+                .Project(x => x)
+                .Sink();
+            
+            Assert.True(disposed);
+        }
+    }
+}
