a07c1541519a86bbecc934128af4ee469e66571b
artur rachwal <artur.rachwal@contractors.roche.com>
2020-05-19 15:13:13 +0200

    Generic IFlowBuilder does not inherit from non-generic IFlowBuilder

--- src/PipeSharp/IFlowBuilder.cs
+++ src/PipeSharp/IFlowBuilder.cs
@@ -7,7 +7,7 @@ namespace PipeSharp
         IFlowBuilder<TFilteringError> WithFilteringError<TFilteringError>();
     }
 
-    public interface IFlowBuilder<TFilteringError> : IFlowBuilder, IOnChangingOnChangedApplier<TFilteringError>
+    public interface IFlowBuilder<TFilteringError> : IOnChangingOnChangedApplier<TFilteringError>
     {
         IFlowBuilderWithEventsApplied<TFilteringError> WithEvents(IEventReceiverFactory eventReceiverFactory);
         IFlow<T, TFilteringError> For<T>(T target);
